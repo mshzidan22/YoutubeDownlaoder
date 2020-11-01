eefef8d03d5747d451bfb488b0e0e2b300525a30
mshzidan22 <mshzidan22@yahoo.com>
2020-11-01 23:33:39 +0200

    Signed-off-by: mshzidan22 <mshzidan22@yahoo.com>

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VideoController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::post('/getLinks', [VideoController::class, 'getLinks']);
+
+
+
+
+
+
